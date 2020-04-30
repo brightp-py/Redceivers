46c84ec4c083a9e7e5208a34c7da2fc9597f1f45
brightp <brightp@umich.edu>
2020-04-30 17:02:19 -0400

    Re-commit

---
+++ src/main/java/com/github/brightp_py/redceivers/Redceivers.java
@@ -0,0 +1,8 @@
+package com.github.brightp_py.redceivers;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Redceivers.MODID)
+public class Redceivers {
+    public static final String MODID = "redceivers";
+}
