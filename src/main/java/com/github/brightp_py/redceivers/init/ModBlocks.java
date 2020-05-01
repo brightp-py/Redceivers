49ab559097e009391686fcd1ef4c16becd32435b
brightp <brightp@umich.edu>
2020-05-01 16:42:20 -0400

    Implemented Transmitter block; changed redceiver textures; added loot_tables, recipes

--- src/main/java/com/github/brightp_py/redceivers/init/ModBlocks.java
+++ src/main/java/com/github/brightp_py/redceivers/init/ModBlocks.java
@@ -2,6 +2,7 @@ package com.github.brightp_py.redceivers.init;
 
 import com.github.brightp_py.redceivers.Redceivers;
 import com.github.brightp_py.redceivers.block.RedceiverBlock;
+import com.github.brightp_py.redceivers.block.TransmitterBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -14,4 +15,5 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Redceivers.MODID);
 
     public static final RegistryObject<Block> REDCEIVER_BLOCK = BLOCKS.register("redceiver", () -> new RedceiverBlock(Block.Properties.create(Material.ROCK, MaterialColor.RED).hardnessAndResistance(3.0F, 3.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> TRANSMITTER_BLOCK = BLOCKS.register("transmitter", () -> new TransmitterBlock(Block.Properties.create(Material.ROCK, MaterialColor.RED).hardnessAndResistance(3.0F, 3.0F).sound(SoundType.METAL)));
 }
